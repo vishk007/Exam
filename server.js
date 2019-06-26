89d37ebd98730a5b6f82ede59f7030c7b1917103
dac <dac@dypm.org>
2019-06-26 07:43:37 +0530

    first

--- server.js
+++ server.js
@@ -1,44 +1,21 @@
-var express =require('http')
-var product = [{
-	id: 11
-	name:'vishal'
-	age: 25
-},
-{
-	id: 12
-	name:"akshay"
-	age: 24
-},
-{
-	id: 13
-	name:"dev"
-	age: 23
-},
-{
-	id: 14
-	name:'sanket'
-	age: 22
-},
+var express=require('express');
+var appln=express();
 
-]
-var customerController=function (req, res) {
-  console.log("CAlling rest api");
-  var abc=[
-            {empId:'11',empName:'vishal',age:25},
-			 {empId:'11',empName:'akshay',age:24},
-			  {empId:'11',empName:'dev',age:23},
-			  {empId:'11',empName:'sanket',age:22}
-			  
-            
-      ];
-  res.send(xyz);
+var gatherInformation=function(req,resp)
+{
+console.log('hello');
+var accinfo=[
+	{accountid:1,customername:'sushrut',company:'Infosys',bill:4000000},
+	{accountid:2,customername:'sush',company:'HSBC',bill:6000000},
+	{accountid:3,customername:'abc',company:'Deutshe Bank',bill:80000000}
+	];
+	resp.send(accinfo);
 };
 
-app.get('/xyz',customerController );
+appln.get('/acc',gatherInformation);
 
-var server = app.listen(8082, function () {
+var server = appln.listen(8089, function () {
   var host = server.address().address
   var port = server.address().port
-
-  console.log("Example app listening at http://localhost:8082", host, port)
+  console.log("port is 8089", host, port)
 })
